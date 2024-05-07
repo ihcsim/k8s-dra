21e5fa863ecc08db15eafd37ea308687164e0462
Ivan Sim <ihcsim@gmail.com>
2024-05-07 13:20:44 -0700

    Move all API types definition to types.go
    Signed-off-by: Ivan Sim <ihcsim@gmail.com>

--- pkg/apis/gpu/v1alpha1/types.go
+++ pkg/apis/gpu/v1alpha1/types.go
@@ -35,3 +35,31 @@ type DeviceClassParametersList struct {
 
 	Items []DeviceClassParameters `json:"items"`
 }
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +k8s:openapi-gen=true
+// +kubebuilder:resource:scope=Namespaced
+
+// ResourceClaimParameters holds the set of parameters provided when creating a resource claim for a GPU.
+type ResourceClaimParameters struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec ResourceClaimParametersSpec `json:"spec,omitempty"`
+}
+
+// ResourceClaimParametersSpec is the spec for the ResourceClaimParameters CRD.
+type ResourceClaimParametersSpec struct {
+	Count int `json:"count,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ResourceClaimParametersList represents the "plural" of a ResourceClaimParameters CRD object.
+type ResourceClaimParametersList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []ResourceClaimParameters `json:"items"`
+}
